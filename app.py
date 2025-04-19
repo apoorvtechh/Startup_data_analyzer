78c7e159d42dec5b8baec3638d945287eb0e6c9e
apoorvtechh <apoorvtechh@gmail.com>
2025-04-20 01:40:10 +0530

    added content in startup page

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import seaborn as sns
+import calendar
+
 
 
 
@@ -11,6 +14,8 @@ df['subvertical'] = df['subvertical'].fillna('Not Specified')
 df['date']=pd.to_datetime(df['date'])
 df['year'] = df['date'].dt.year
 df['month']=df['date'].dt.month
+df['month'] = df['month'].astype('Int64')
+df['year'] = df['year'].astype('Int64')
 count_df=df.groupby(['year','month'])['startup'].count().reset_index()
 count_df['month']=count_df['month'].astype('int')
 count_df['year']=count_df['year'].astype('int')
@@ -42,7 +47,9 @@ def load_investor_detail(investor):
         if  not city_df.empty and city_df.sum() > 0:
             st.subheader(f'City Details - {investor}')
             fig3, ax3 = plt.subplots()
-            ax3.pie(city_df,labels=city_df.index,autopct="%0.01f%%")
+            ax3.pie(city_df,labels=None,autopct="%0.01f%%")
+            ax3.legend( labels=city_df.index, title="City", loc="center left", bbox_to_anchor=(1, 1))
+            
             st.pyplot(fig3)
         else:
             st.warning(f"No investment data available to plot for investor: {investor}")
@@ -56,12 +63,6 @@ def load_investor_detail(investor):
         else:
             st.warning(f"No investment data available to plot for investor: {investor}")
 
-
-
-
-
-
-
     with col2:
         vertical_df = df[df['investors'].str.contains(investor, na=False)].groupby('vertical')['amount'].sum()
         round_df = df[df['investors'].str.contains(investor, na=False)].groupby('round')['amount'].sum()
@@ -71,7 +72,9 @@ def load_investor_detail(investor):
         if  not vertical_df.empty and vertical_df.sum() > 0:
             st.subheader(f'Investment by Vertical - {investor}')
             fig1, ax1 = plt.subplots(figsize=(12,12))
-            ax1.pie(vertical_df,labels=vertical_df.index,autopct="%0.01f%%")
+            ax1.pie(vertical_df,labels=None,autopct="%0.01f%%")
+            ax1.legend( labels=vertical_df.index, title="Verticals", loc="center left", bbox_to_anchor=(1, 1))
+            
             st.pyplot(fig1)
         else:
             st.warning(f"No investment data available to plot for investor: {investor}")
@@ -79,7 +82,8 @@ def load_investor_detail(investor):
         if  not round_df.empty and round_df.sum() > 0:
             st.subheader(f'Investment Rounds - {investor}')
             fig2, ax2 = plt.subplots(figsize=(8,8))
-            ax2.pie(round_df,labels=round_df.index,autopct="%0.01f%%")
+            ax2.pie(round_df,labels=None,autopct="%0.01f%%")
+            ax2.legend( labels=round_df.index, title="Rounds", loc="center left", bbox_to_anchor=(1, 1))
             st.pyplot(fig2)
         else:
             st.warning(f"No investment data available to plot for investor: {investor}")
@@ -138,7 +142,76 @@ def Overall_analysis():
             labels=total_amount_startup_df.index,
             autopct="%0.1f%%")
         st.pyplot(fig4)
+    col5,col6=st.columns(2)
+    with col5:
+        st.subheader('Top 10 Round Based on Investments')
+        round_dff=df.groupby('round')['amount'].sum().sort_values(ascending=False).head(10)
+        fig5, ax5 = plt.subplots(figsize=(10, 10))
+        ax5.pie(
+            round_dff,
+            labels=round_dff.index,
+            autopct="%0.1f%%")
+        st.pyplot(fig5)
+    with col6:
+        st.subheader('Top 10 City Based on Investments')
+        city_dff=df.groupby('city')['amount'].sum().sort_values(ascending=False).head(10)
+        fig6,ax6=plt.subplots(figsize=(10,7.5))
+        ax6.pie(city_dff,labels=city_dff.index,autopct="%0.1f%%")
+        st.pyplot(fig6)
+    st.subheader('Top Startup Year-Wise Based On Amount Invested')
+    year_wise_startup=df[df['amount'] == df.groupby('year')['amount'].transform('max')].reset_index(drop=True).drop(columns=['Sr No'])
+    st.dataframe(year_wise_startup)
+    # Group and pivot the data
+    heatmap_data = df.groupby(['year', 'month'])['amount'].sum().unstack().fillna(0)
+   
+    fig7, ax7 = plt.subplots(figsize=(12, 6))
+    sns.heatmap(heatmap_data, annot=True, fmt=".0f", cmap="YlGnBu", ax=ax7)
+
+    ax7.set_title("Total Funding Amount by Year and Month")
+    ax7.set_xlabel("Month")
+    ax7.set_ylabel("Year")
+    st.pyplot(fig7)
+
+def load_startup_setails(startup):
+    st.title(startup)
+    df['date'] = pd.to_datetime(df['date']).dt.date
+    last5df=df[df['startup'].str.contains(startup, na=False)][['date','investors','vertical','subvertical','city','round','amount','year']].reset_index().drop(columns=['index'])
+    st.subheader('Most Recent Investments')
+    st.dataframe(last5df)
+    col1,col2=st.columns(2)
+    with col1:
+        round_inv=last5df.groupby('round')['amount'].sum()
+        if  not round_inv.empty and round_inv.sum()>0:
+            st.subheader(f'Investment Made Upon Rounds- {startup}')
+            fig1, ax1 = plt.subplots(figsize=(10,5))
+            ax1.pie(round_inv,labels=None,autopct="%0.01f%%")
+            ax1.legend( labels=round_inv.index, title="Funding Rounds", loc="center left", bbox_to_anchor=(1, 1))
+            st.pyplot(fig1)
+        else:
+            st.warning(f"No investment data available to plot for startup: {startup}")
+    with col2:
+        investor_wise=last5df.groupby('investors')['amount'].sum()
+        if  not investor_wise.empty and investor_wise.sum()>0:
+            st.subheader(f'Investment Made By Investors- {startup}')
+            fig2, ax2 = plt.subplots(figsize=(10,15))
+            ax2.pie(investor_wise,labels=None,autopct="%0.01f%%")
+            ax2.legend( labels=investor_wise.index, title="Investors", loc="center left", bbox_to_anchor=(1, 1))
+            st.pyplot(fig2)
+        else:
+            st.warning(f"No investment data available to plot for startup: {startup}")
+    st.header('Date Wise Investments')
+    date_wise=last5df.groupby('year')['amount'].sum()
+    date_wise.index=date_wise.index.astype(int)
+    fig3,ax3=plt.subplots(figsize=(14, 6))
+    ax3.bar(date_wise.index,date_wise.values)
+    ax3.set_xticks(ax3.get_xticks()[::1])
+    st.pyplot(fig3)
+   
     
+        
+        
+
+        
 
 
 
@@ -156,9 +229,11 @@ if option=='Overall Analysis':
 
 
 elif option=='Startup':
-    st.sidebar.selectbox('Select Startup',sorted(df['startup'].unique().tolist()))
+    selected_stratup=st.sidebar.selectbox('Select Startup',sorted(df['startup'].unique().tolist()))
     btn1=st.sidebar.button('Find Starup Details')
-    st.title('Startup Analysis')
+    if btn1:
+        load_startup_setails(selected_stratup)
+        
 else:
     selected_investor= st.sidebar.selectbox('Select Investor',sorted(set(df['investors'].fillna('').astype(str).str.split(',').sum())))
     btn2=st.sidebar.button('Find Investor Detail')
